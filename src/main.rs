39f47ef716a9dba025f1e8ec0ae6e0f28d55e0f1
Adam French <adam.a.french@outlook.com>
2026-02-18 13:44:30 +0000

    Add commands and options

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use std::error::Error;
+use std::path::PathBuf;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // Create a new tour
+    Init {
+        #[arg(short, long, value_name = "MESSAGE")]
+        message: Option<String>,
+    },
+    // Add steps to the tour
+    Commit {
+        files: Vec<PathBuf>,
+
+        #[arg(short, long, value_name = "MESSAGE")]
+        message: Option<String>,
+    },
+    // Finish the tour
+    End {
+        #[arg(short, long, value_name = "MESSAGE")]
+        message: Option<String>,
+    },
+
+    // Go to next step of tour
+    Next {
+        #[arg(short, value_name = "NUM STEPS")]
+        n: Option<i32>,
+    },
+    // Go to previous step of tour
+    Prev {
+        #[arg(short, value_name = "NUM STEPS")]
+        n: Option<i32>,
+    },
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    Ok(())
 }
